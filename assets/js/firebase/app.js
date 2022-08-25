9df3183bb07d2f72682581e9ce497c0b40919558
Camila <camila.matosrocha1234@gmail.com>
2022-08-25 15:23:44 -0300

    Integração com o Firebase para preenchimento do formulário

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBWAQSbOJ8OM5y3WlLf_m1D1Zqd2GXMb3Q",
+    authDomain: "mundo-invertido-a8812.firebaseapp.com",
+    projectId: "mundo-invertido-a8812",
+    storageBucket: "mundo-invertido-a8812.appspot.com",
+    messagingSenderId: "269683257148",
+    appId: "1:269683257148:web:6d40a0f081e16f7361b839"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  export default app
